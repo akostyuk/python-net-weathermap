74ff93198e99095f77157083ddb5d2fe8bb2b6a7
Alexey Kostyuk <unitoff@gmail.com>
2011-02-25 12:01:06 +0300

    Added setup.py script

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='net_weathermap',
+      version='0.1',
+      description='Python tool visualizes network utilization using MRTG data aggregation',
+      author='Alexey Kostyuk',
+      author_email='unitoff+pynetweathermap@gmail.com',
+      url='https://github.com/akostyuk/python-net-weathermap',
+      packages=['net_weathermap'],
+     )
